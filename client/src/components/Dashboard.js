445fc67f11731a6c303c9db213a65e935845f13f
jurusteve <jstevemu17@gmail.com>
2019-05-14 23:55:30 -0400

    completed mvp

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Dashboard = () => {
+const Dashboard = ({
+  updateBallCount,
+  addFoul,
+  updateStrikeCount,
+  resetCounts
+}) => {
   return (
     <div>
       <h3>Dashboard</h3>
       <div className="btn">
-        <button>Update</button>
+        <button data-testid="ball-count" onClick={updateBallCount}>
+          Balls
+        </button>
+        <button onClick={resetCounts}>Hit</button>
+        <button onClick={addFoul}>Foul</button>
+        <button onClick={updateStrikeCount}>Strike</button>
       </div>
     </div>
   );
--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "./styles.css";
-const Display = () => {
+const Display = props => {
+  const {
+    stats: { ballCount, strikeCount }
+  } = props;
   return (
     <div>
       <h3>Display</h3>
       <div className="count">
         <div className="balls">
           <h5>Balls Count</h5>
-          <p>0</p>
+          <p data-testid="balls-count">{ballCount}</p>
         </div>
         <div className="stikes">
           <h5>Strikes Count</h5>
-          <p>0</p>
+          <p data-testid="strikes-count">{strikeCount}</p>
         </div>
       </div>
     </div>
